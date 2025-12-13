0b3324983544e49b5a592128f2ca028cbd4ce3c1
Sujal Shrestha <shresthasujal519@gmail.com>
2025-12-14 00:21:55 +0545

    Controller test cases

--- src/test/java/com/taskmanagementsystem/task_management/Controllers/TaskControllerTest.java
+++ src/test/java/com/taskmanagementsystem/task_management/Controllers/TaskControllerTest.java
@@ -3,6 +3,7 @@ package com.taskmanagementsystem.task_management.Controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.taskmanagementsystem.task_management.DTOs.AddTaskDTO;
+import com.taskmanagementsystem.task_management.DTOs.UpdateTaskDTO;
 import com.taskmanagementsystem.task_management.Models.Tasks;
 import com.taskmanagementsystem.task_management.Services.Implementations.TaskService;
 import org.junit.jupiter.api.Test;
@@ -18,8 +19,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -119,5 +119,121 @@ public class TaskControllerTest {
         verify(taskService, times(1)).getTaskById(taskId);
     }
 
+    @Test
+    public void shouldUpdateTaskSuccessfully() throws Exception {
+        long taskId = 1L;
 
+        UpdateTaskDTO updateTaskDTO = UpdateTaskDTO.builder()
+                .title("Updated Task")
+                .description("This is the updated task description")
+                .dueDate(LocalDate.now().plusDays(2))
+                .status("IN_PROGRESS")
+                .build();
+
+        Tasks updatedTask = Tasks.builder()
+                .id(taskId)
+                .title(updateTaskDTO.getTitle())
+                .description(updateTaskDTO.getDescription())
+                .dueDate(updateTaskDTO.getDueDate())
+                .createDate(LocalDate.now())
+                .status(Tasks.Status.valueOf(updateTaskDTO.getStatus()))
+                .build();
+
+        when(taskService.updateTask(eq(taskId), any(UpdateTaskDTO.class))).thenReturn(updatedTask);
+
+        mockMvc.perform(put("/api/tasks/update-task/{taskId}", taskId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updateTaskDTO))
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value(updateTaskDTO.getTitle()))
+                .andExpect(jsonPath("$.description").value(updateTaskDTO.getDescription()))
+                .andExpect(jsonPath("$.dueDate").value(updateTaskDTO.getDueDate().toString()))
+                .andExpect(jsonPath("$.status").value(updateTaskDTO.getStatus()));
+        verify(taskService, times(1)).updateTask(eq(taskId), any(UpdateTaskDTO.class));
+    }
+
+    @Test
+    public void shouldUpdateTaskStatusSuccessfully() throws Exception {
+        long taskId = 1L;
+        String status = "IN_PROGRESS";
+
+        Tasks updatedTask = Tasks.builder()
+                .id(taskId)
+                .title("Task 1")
+                .description("This is task 1")
+                .dueDate(LocalDate.now().plusDays(1))
+                .createDate(LocalDate.now())
+                .status(Tasks.Status.valueOf(status))
+                .build();
+
+        when(taskService.updateTaskStatus(taskId, status)).thenReturn(updatedTask);
+
+        mockMvc.perform(patch("/api/tasks/update-task-status/{taskId}", taskId)
+                .param("status", status)
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(status));
+
+        verify(taskService, times(1)).updateTaskStatus(taskId, status);
+    }
+
+    @Test
+    public void shouldDeleteTaskSuccessfully() throws Exception {
+        long taskId = 1L;
+        doNothing().when(taskService).deleteTask(taskId);
+
+        mockMvc.perform(delete("/api/tasks/delete-task/{taskId}", taskId))
+                .andExpect(status().isNoContent());
+
+        verify(taskService, times(1)).deleteTask(taskId);
+    }
+
+    @Test
+    public void shouldDeleteAllTasksSuccessfully() throws Exception {
+        doNothing().when(taskService).deleteAllTasks();
+
+        mockMvc.perform(delete("/api/tasks/delete-all-tasks"))
+                .andExpect(status().isNoContent());
+
+        verify(taskService, times(1)).deleteAllTasks();
+    }
+
+    @Test
+    public void shouldGetTaskByStatusSuccessfully() throws Exception {
+        String status = "PENDING";
+        List<Tasks> tasksList  = List.of(
+                Tasks.builder()
+                        .id(1L)
+                        .title("Task 1")
+                        .description("This is task 1")
+                        .dueDate(LocalDate.now().plusDays(1))
+                        .createDate(LocalDate.now())
+                        .status(Tasks.Status.PENDING)
+                        .build(),
+
+                Tasks.builder()
+                        .id(2L)
+                        .title("Task 2")
+                        .description("This is task 2")
+                        .dueDate(LocalDate.now().plusDays(2))
+                        .createDate(LocalDate.now())
+                        .status(Tasks.Status.PENDING)
+                        .build()
+        );
+
+        when(taskService.getTasksByStatus(status)).thenReturn(tasksList);
+
+        mockMvc.perform(
+                get("/api/tasks/get-tasks-by-status")
+                        .param("status", status)
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].title").value(tasksList.get(0).getTitle()))
+                .andExpect(jsonPath("$.length()").value(tasksList.size()));
+
+        verify(taskService, times(1)).getTasksByStatus(status);
+    }
 }
